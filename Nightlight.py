a9a2365d4820b97fa303d314283f68c3a43b28bf
amro125 <amitrogel97@gmail.com>
2021-11-30 06:43:01 -0500

    Create Nightlight.py

---
+++ Nightlight.py
@@ -0,0 +1,35 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def oneToOne(argument):
+    v = q1to1.get()
+
+
+def oneToOneStart(argument):
+    arm.setjointangles(joint = [02873490381],speed = [982371])
+
+def sequencer(argument):
+
+def startSequencer(argument):
+
+def MappingtoUse(argument):
+    switcher = {
+        0: "zero",
+        1: "one",
+        2: "two",
+    }
+
+    # get() method of dictionary data type returns
+    # value of passed argument if it is present
+    # in dictionary otherwise second argument will
+    # be assigned as default value of passed argument
+    return switcher.get(argument, "nothing")
+
+
+# Driver program
+if __name__ == "__main__":
+    argument = 0
+    print(MappingtoUse(argumen
